8c9ea66367d989622c173f3dfa010d0fcf42f817
Громова Екатерина <ekgromova269@gmail.com>
2026-03-16 19:11:14 +0300

    добавлены геттеры и сеттеры для всех полей класса

--- classes/Machine.dart
+++ classes/Machine.dart
@@ -13,4 +13,42 @@ class Machine {
     print('Денег: $_cash руб');
     print('------------------------');
   }
+
+  int get coffeeBeans => _coffeeBeans;
+  int get milk => _milk;
+  int get water => _water;
+  double get cash => _cash;
+
+  set coffeeBeans(int value) {
+    if (value >= 0) {
+      _coffeeBeans = value;
+    } else {
+      print('Ошибка: количество кофе не может быть отрицательным');
+    }
+  }
+
+  set milk(int value) {
+    if (value >= 0) {
+      _milk = value;
+    } else {
+      print('Ошибка: количество молока не может быть отрицательным');
+    }
+  }
+
+  set water(int value) {
+    if (value >= 0) {
+      _water = value;
+    } else {
+      print('Ошибка: количество воды не может быть отрицательным');
+    }
+  }
+
+  set cash(double value) {
+    if (value >= 0) {
+      _cash = value;
+    } else {
+      print('Ошибка: количество денег не может быть отрицательным');
+    }
+  }
+
 }
